bce8765400e6f9083d671632fb36e597a3de10d8
giulio8 <giuliosichili@gmail.com>
2025-07-24 17:22:51 +0200

    docker run detatched, accessible from visual studio code

--- docker-run-interactive.sh
+++ docker-run-interactive.sh
@@ -5,13 +5,11 @@ CODE_DIR="/home/giulio/XAI-AUTOMI-seg/code"
 DATA_DIR="/home/giulio/XAI-AUTOMI-seg/data"
 OUTPUT_DIR="/home/giulio/XAI-AUTOMI-seg/output"
 
-
-docker run -dit \
-  --name automi-dev \
-  --gpus device=0 \
+# Esegui il container montando i volumi
+docker run -it --gpus device=0 --rm \
+  --runtime=nvidia \
   -v "$CODE_DIR":/workspace/code \
   -v "$DATA_DIR/Dataset003_AUTOMI_CTVLNF_NEWGL_raw_data":/workspace/data/nnunet_raw \
   -v "$DATA_DIR/Dataset003_AUTOMI_CTVLNF_NEWGL_results":/workspace/data/results \
   -v "$OUTPUT_DIR":/workspace/output \
-  -w /workspace \
   automi-base
